16816972fd5587731f2dc6126ed227a23b4df718
aliM <alozicyber@gmail.com>
2019-10-08 23:41:56 +0200

    video 12 done, current.value

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,20 +1,32 @@
-import React from 'react';
-
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  render(){
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  render() {
     return (
       <React.Fragment>
-      <form action="" className="store-selector">
-        <h2> Please Enter A Store </h2>
-        {/* {  block comment should be inside html tag } */}
-        <input type="text" required palceholder="Store Name" />
-        <button type="submit"> Visit Store →</button>
-      </form>
+        <form action="" className="store-selector" onSubmit={this.goToStore}>
+          <h2> Please Enter A Store </h2>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            palceholder="Store Name"
+            defaultValue={getFunName()}
+          />
+          <button type="submit"> Visit Store →</button>
+        </form>
       </React.Fragment>
-    )
+    );
   }
 }
-
 
 export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react"; // here we have to import react each time
-import { render } from "react-dom";  // render method taken from library
-import StorePicker from "./components/StorePicker";
-import App from "./components/App";
+import { render } from "react-dom"; // render method taken from library
+import Router from "./components/Router";
 import "./css/style.css";
 
-
-
-render(<App/>,  document.querySelector('#main'));
+render(<Router />, document.querySelector("#main"));
